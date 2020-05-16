eb4f9f661b2c15b635c9f0bac4c22329d4e64b4c
pd4184 <pd4184@gmail.com>
2020-05-16 09:16:25 +0530

    mongoAtlas using

--- modules/user.js
+++ modules/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://localhost:27017/pms', {useNewUrlParser: true, useCreateIndex: true});
- mongoose.connect('mongodb://pd4184:pd41849630@172.30.168.219:27017/sampledb', {useNewUrlParser: true, useCreateIndex: true});
+ //mongoose.connect('mongodb://pd4184:pd41849630@172.30.168.219:27017/sampledb', {useNewUrlParser: true, useCreateIndex: true});
+ 
+mongoose.connect('mongodb+srv://pd4184:pd41849630@pandu-m7ddl.mongodb.net/pmdb',{useNewUrlParser: true, useCreateIndex: true});
 var conn = mongoose.Collection;
 var userSchema = new mongoose.Schema({
     username: {
